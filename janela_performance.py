c98a5bb727f47c5edc7e17fbeb15733519155bdb
tomas <tomas.monteiro150@gmail.com>
2023-06-28 17:58:27 +0100

    Initial commit

--- janela_performance.py
+++ janela_performance.py
@@ -11,14 +11,61 @@ mydb = mysql.connector.connect(
 )
 
 def mostrar_informacoes_aluno(aluno):
-    messagebox.showinfo("Informações do Aluno", f"ID do Usuário: {aluno[0]}\nNome: {aluno[1]}\nCurso: {aluno[2]}")
+    aluno_id = aluno[0]
+    aluno_nome = aluno[1]
+    aluno_curso = aluno[2]
+    aluno_curso_id = aluno[3]
+
+    cursor = mydb.cursor()
+
+    # Média das notas do aluno no curso
+    cursor.execute("""
+    SELECT AVG(q_avaliacoes.avaliacao_nota)
+    FROM q_alunos_cursos
+    JOIN q_avaliacoes ON q_alunos_cursos.curso_id = q_avaliacoes.avaliacao_curso
+    WHERE q_alunos_cursos.aluno_id = %s AND q_alunos_cursos.curso_id = %s
+    """, (aluno_id, aluno_curso_id))
+    aluno_media = cursor.fetchone()[0]
+
+    # Média das notas do curso
+    cursor.execute("""
+    SELECT AVG(q_avaliacoes.avaliacao_nota)
+    FROM q_avaliacoes
+    WHERE q_avaliacoes.avaliacao_curso = %s
+    """, (aluno_curso_id,))
+    curso_media = cursor.fetchone()[0]
+
+    # Nota máxima do curso
+    cursor.execute("""
+    SELECT MAX(q_avaliacoes.avaliacao_nota)
+    FROM q_avaliacoes
+    WHERE q_avaliacoes.avaliacao_curso = %s
+    """, (aluno_curso_id,))
+    nota_maxima = cursor.fetchone()[0]
+
+    # Nota mínima do curso
+    cursor.execute("""
+    SELECT MIN(q_avaliacoes.avaliacao_nota)
+    FROM q_avaliacoes
+    WHERE q_avaliacoes.avaliacao_curso = %s
+    """, (aluno_curso_id,))
+    nota_minima = cursor.fetchone()[0]
+
+    messagebox.showinfo("Informações do Aluno", f"ID do Usuário: {aluno_id}\nNome: {aluno_nome}\nCurso: {aluno_curso}\nMédia das Notas do Aluno: {aluno_media:.2f}\nMédia das Notas do Curso: {curso_media:.2f}\nNota Máxima do Curso: {nota_maxima:.2f}\nNota Mínima do Curso: {nota_minima:.2f}")
 
 def gestao_performance(content_frame):
     def carregar_alunos():
         tree.delete(*tree.get_children())
 
         cursor = mydb.cursor()
-        cursor.execute("SELECT * FROM q_utilizadores")
+        cursor.execute("""SELECT u.utilizador_id AS aluno_id, u.utilizador_nome AS aluno, c.curso_desc AS curso, c.curso_id, COUNT(*) AS num_avaliacoes
+FROM q_avaliacoes a
+JOIN q_utilizadores u ON a.avaliacao_aluno_id = u.utilizador_id
+JOIN q_cursos c ON a.avaliacao_curso = c.curso_id
+GROUP BY u.utilizador_id, u.utilizador_nome, c.curso_desc, c.curso_id
+HAVING COUNT(*) >= 2
+ORDER BY u.utilizador_nome;
+""")
         alunos = cursor.fetchall()
 
         for aluno in alunos:
@@ -33,9 +80,7 @@ def gestao_performance(content_frame):
     for widget in content_frame.winfo_children():
         widget.destroy()
 
-    tree = ttk.Treeview(content_frame, columns=("ID", "Nome", "Curso"))
-    tree.heading("#0", text="ID")
-    tree.column("#0", width=50)
+    tree = ttk.Treeview(content_frame, columns=("ID", "Nome", "Curso"), show="headings")
     tree.heading("ID", text="ID")
     tree.column("ID", width=100)
     tree.heading("Nome", text="Nome")
