5c0e1770fa93ab6d966d6787a2d7b17653389924
tomas <tomas.monteiro150@gmail.com>
2023-06-30 13:45:36 +0100

    Initial commit

--- janela_avaliacao.py
+++ janela_avaliacao.py
@@ -14,20 +14,25 @@ def gestao_avaliacoes(content_frame):
         tree.delete(*tree.get_children())
 
         cursor = mydb.cursor()
-        cursor.execute("SELECT * FROM q_avaliacoes")
+        cursor.execute("""SELECT u.utilizador_nome AS nome_aluno, c.curso_desc AS nome_curso, a.avaliacao_data, a.avaliacao_nota
+FROM q_avaliacoes a
+JOIN q_utilizadores u ON a.avaliacao_aluno_id = u.utilizador_id
+JOIN q_cursos c ON a.avaliacao_curso = c.curso_id
+""")
         avaliacoes = cursor.fetchall()
 
         for avaliacao in avaliacoes:
-            tree.insert("", "end", values=avaliacao[1:])  # Ignorando a primeira coluna (ID)
+            tree.insert("", "end", values=avaliacao)
 
     def adicionar_avaliacao():
         aluno = entry_aluno.get()
         disciplina = entry_disciplina.get()
         nota = entry_nota.get()
+        data = entry_data.get()
 
         cursor = mydb.cursor()
-        query = "INSERT INTO q_avaliacoes (avaliacao_aluno_id, avaliacao_data, avaliacao_nota) VALUES (%s, %s, %s)"
-        values = (aluno, disciplina, nota)
+        query = "INSERT INTO q_avaliacoes (avaliacao_aluno_id, avaliacao_curso, avaliacao_nota, avaliacao_data) VALUES (%s, %s, %s, %s)"
+        values = (aluno, disciplina, nota, data)
         cursor.execute(query, values)
         mydb.commit()
 
@@ -37,15 +42,18 @@ def gestao_avaliacoes(content_frame):
         selected_item = tree.focus()
         if selected_item:
             avaliacao = tree.item(selected_item)["values"]
+            data, aluno, disciplina, nota = avaliacao
+
             id_avaliacao = avaliacao[0]
 
             aluno = entry_aluno.get()
             disciplina = entry_disciplina.get()
             nota = entry_nota.get()
+            data = entry_data.get()
 
             cursor = mydb.cursor()
-            query = "UPDATE q_avaliacoes SET avaliacao_aluno = %s, avaliacao_disciplina = %s, avaliacao_nota = %s WHERE avaliacao_id = %s"
-            values = (aluno, disciplina, nota, id_avaliacao)
+            query = "UPDATE q_avaliacoes SET avaliacao_aluno_id = %s, avaliacao_curso = %s, avaliacao_nota = %s, avaliacao_data = %s WHERE avaliacao_id = %s"
+            values = (aluno, disciplina, nota, data, id_avaliacao)
             cursor.execute(query, values)
             mydb.commit()
 
@@ -68,11 +76,13 @@ def gestao_avaliacoes(content_frame):
     for widget in content_frame.winfo_children():
         widget.destroy()
 
-    tree = ttk.Treeview(content_frame, columns=("Aluno", "Disciplina", "Nota"), show="headings")
+    tree = ttk.Treeview(content_frame, columns=("Aluno", "Curso", "Data", "Nota"), show="headings")
     tree.heading("Aluno", text="Aluno")
     tree.column("Aluno", width=150)
-    tree.heading("Disciplina", text="Curso")
-    tree.column("Disciplina", width=150)
+    tree.heading("Curso", text="Curso")
+    tree.column("Curso", width=150)
+    tree.heading("Data", text="Data")
+    tree.column("Data", width=100)
     tree.heading("Nota", text="Nota")
     tree.column("Nota", width=50)
 
@@ -82,6 +92,11 @@ def gestao_avaliacoes(content_frame):
     tree.pack(expand=True, fill="both")
 
     carregar_avaliacoes()
+
+    label_data = Label(content_frame, text="Data:")
+    label_data.pack()
+    entry_data = Entry(content_frame)
+    entry_data.pack()
 
     label_aluno = Label(content_frame, text="Aluno:")
     label_aluno.pack()
